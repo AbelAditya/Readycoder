76736b6642e66e8a24600858cb2e990a1ad41744
AbelAditya <109233117+AbelAditya@users.noreply.github.com>
2022-08-30 07:54:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const morgon = require("morgan");
+const cors = require("cors");
+const userModel = require("./models/userDetails");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgon("dev"));
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.status(200).json({
+    status: "success",
+    message: "Request Received",
+  });
+});
+
+app
+  .route("/api/v1/user")
+  .get((request, response) => {
+    console.log("Request Received");
+
+    response.status(200).json({
+      status: "success",
+      message: "Request Received",
+    });
+  })
+  .post(async (request, response) => {
+    try {
+      let userDetails = await userModel.create(request.body);
+
+      response.status(200).json({
+        status: "success",
+        data: userDetails,
+      });
+    } catch (err) {
+      response.status(400).json({
+        status: "fail",
+        message: err,
+      });
+    }
+  });
+
+module.exports = app;
